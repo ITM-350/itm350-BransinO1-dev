60ca6b871e5317e964f1a7d8936111ddb2c62d5b
BransinO1 <corbinmeacham65@gmail.com>
2024-12-17 15:08:29 -0700

    finalcoverage

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
-// For every .js file in the src directory, require at least 80% line coverage.
 module.exports = {
   coverageThreshold: {
-    './src/*.js': {
+    global: {
+      lines: 80,       // Global requirement for all files
+      statements: 80,  // Ensures statement coverage
+      branches: 80,    // Ensures branch coverage
+      functions: 80,   // Ensures function coverage
+    },
+    './src/**/*.js': { // Per-file requirement for the src directory
       lines: 25
     }
   }
-}
+};
